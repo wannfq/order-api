50b5ad9578ccd048eb48cdc9308d9d40155bd1e1
Wan Afiq <wannfq@gmail.com>
2020-07-17 18:34:27 +0800

    Adding dotenv

---
+++ README.md
@@ -0,0 +1,3 @@
+# Order API
+
+[https://evening-stream-08538.herokuapp.com/](https://evening-stream-08538.herokuapp.com/)
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import express from 'express';
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", (req, res) =>
+  res.json({ statusCode: 200, message: "OK" } as IResponse),
+);
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+
+export interface IResponse {
+  data?: any;
+  message?: string;
+  statusCode: number;
+}
